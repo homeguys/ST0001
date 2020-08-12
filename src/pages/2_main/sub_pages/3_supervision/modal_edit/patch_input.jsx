c38afe118af381f716f248685c4e2dbb20c91944
wanglei <918061733@qq.com>
2020-08-12 21:26:21 +0800

    add page

--- src/pages/2_main/sub_pages/3_supervision/modal_edit/patch_input.jsx
+++ src/pages/2_main/sub_pages/3_supervision/modal_edit/patch_input.jsx
@@ -7,6 +7,24 @@ function PatchInput() {
         图斑录入
         <i />
       </section>
+      <section className="upload">
+        <section className="upload-item">
+          <span className="upload-item-title">占用水域图斑录入</span>
+          <section className="upload-content">
+            <span className="text">aa</span>
+            <span className="btn">上传</span>
+            <input type="file" />
+          </section>
+        </section>
+        <section className="upload-item">
+          <span className="upload-item-title">占用水域图斑录入</span>
+          <section className="upload-content">
+            <span className="text">aa</span>
+            <span className="btn">上传</span>
+            <input type="file" />
+          </section>
+        </section>
+      </section>
     </section>
   )
 }
